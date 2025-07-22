b9209bb63d68edf43fd17a57b0c109e24d56718b
linbingcheng <3078421415@qq.com>
2025-07-22 16:24:55 +0800

    sse

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,34 +1,12 @@
-import { AppService } from './app.service';
-import {
-  Controller,
-  Get,
-  Post,
-  StreamableFile,
-  UploadedFiles,
-  UseInterceptors,
-} from '@nestjs/common';
-import { FilesInterceptor } from '@nestjs/platform-express';
-import { createReadStream } from 'fs';
-import { join } from 'path';
-import * as process from 'node:process';
+import { Controller, Sse } from '@nestjs/common';
+import { interval, map, Observable } from 'rxjs';
 
-@Controller('file')
+@Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Post('upload')
-  @UseInterceptors(FilesInterceptor('files'))
-  uploadFile(@UploadedFiles() files: Express.Multer.File[]) {
-    console.log(`共上传了 ${files.length} 个文件`);
-    files.forEach((file, i) => {
-      console.log(`第 ${i + 1} 个文件名: ${file.originalname}`);
-    });
-  }
-
-  @Get('download')
-  getFile(): StreamableFile {
-    // console.log(process.cwd());
-    const file = createReadStream(join(process.cwd(), 'package.json'));
-    return new StreamableFile(file);
+  @Sse('sse')
+  sse(): Observable<MessageEvent> {
+    return interval(1000).pipe(
+      map(() => ({ data: { hello: 'world' } }) as MessageEvent),
+    );
   }
 }
